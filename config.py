61cac21d38c26aaf56c84efc36ab88bbf80e6490
25f3003052 <asmarpranav@gmail.com>
2026-08-07 08:26:54 +0530

    Remove config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-class DevelopmentConfig():
-    DEBUG=True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///Treck.db'
-    SECRET_KEY="secret123"
-    SECURITY_PASSWORD_HASH='bcrypt'
-    SECURITY_PASSWORD_SALT='tree'
